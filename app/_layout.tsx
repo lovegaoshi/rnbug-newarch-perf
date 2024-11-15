be088d10c48b0295465185d5139a3947b89bbb9e
lovegaoshi <106490582+lovegaoshi@users.noreply.github.com>
2024-11-15 05:21:25 -0800

    feat: bug

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,30 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import MotionLayout from "./motionlayout/View";
+import { View } from "react-native";
+import { FlashList } from "@shopify/flash-list";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+const D = () => <View></View>;
+
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -28,12 +38,14 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <View style={{ flex: 1, backgroundColor: "white" }}>
+          <FlashList renderItem={D} />
+        </View>
+        <MotionLayout />
+        <StatusBar style="auto" />
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 }
---
+++ app/motionlayout/Artwork.tsx
@@ -0,0 +1,71 @@
+import { TouchableWithoutFeedback, Dimensions, Image } from "react-native";
+import Animated, {
+  SharedValue,
+  useAnimatedStyle,
+  useDerivedValue,
+} from "react-native-reanimated";
+import { useEffect, useState } from "react";
+const MinPlayerHeight = 75;
+interface Props {
+  miniplayerHeight: SharedValue<number>;
+  opacity: SharedValue<number>;
+  onPress: () => void;
+  expand: () => void;
+}
+
+export default ({ miniplayerHeight, opacity, onPress, expand }: Props) => {
+  const { width } = Dimensions.get("window");
+
+  const artworkWidth = useDerivedValue(() => {
+    return Math.min(miniplayerHeight.value - 25, width);
+  });
+  const artworkBottom = useDerivedValue(() => {
+    const val = miniplayerHeight.value - MinPlayerHeight - 5;
+    const overflowBottom = Math.max(0, miniplayerHeight.value - 100 - width);
+    return Math.min(val - overflowBottom);
+  });
+  const artworkLeft = useDerivedValue(() => {
+    const val = 5 + MinPlayerHeight - miniplayerHeight.value;
+    if (val < 0) return 0;
+    return val;
+  });
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      width: artworkWidth.value,
+      height: artworkWidth.value,
+      bottom: -artworkBottom.value,
+      left: artworkLeft.value,
+      opacity: opacity.value,
+      zIndex: opacity.value > 0 ? 1 : -1,
+    };
+  });
+
+  const onImagePress = () => {
+    if (miniplayerHeight.value === MinPlayerHeight) {
+      return expand();
+    }
+    if (artworkWidth.value === width) {
+      return onPress();
+    }
+  };
+  return (
+    <Animated.View
+      style={[
+        {
+          position: "absolute",
+        },
+        animatedStyle,
+      ]}
+    >
+      <TouchableWithoutFeedback onPress={onImagePress}>
+        <Image
+          style={{ flex: 1 }}
+          source={{
+            uri: "https://rntp.dev/example/Longing.jpeg",
+          }}
+        />
+      </TouchableWithoutFeedback>
+    </Animated.View>
+  );
+};
